873f6ba7f2f2c2a5ec6d1cd3cf99e0d3fc3bc328
cookin32 <cookin32@gmail.com>
2026-05-31 09:31:09 -0500

    Created additional Gaussian Naive-Bayes classificaiton utilizing the sepal length and petal width data set to compare which is a better indicator of classification

--- Classifier/ML_NaiveBayesClassifier.py
+++ Classifier/ML_NaiveBayesClassifier.py
@@ -23,17 +23,31 @@ pwsw_x_train, pwsw_x_test, pwsw_class_train, pwsw_class_test = (
 slpw_x_train, slpw_x_test, slpw_class_train, slpw_class_test = (
     iris.create_train_test_split(x=slpwDF, classification=iris.irisDF['Class'], test_size=.65))
 
+# create gaussian naive bayes classifiers based on petal and sepal width
 pwswGNB = GaussianNB().fit(pwsw_x_train, pwsw_class_train)
 pwsw_y_test_pred = pwswGNB.predict(pwsw_x_test)
 pwswGNB_full = GaussianNB().fit(pwswDF, iris.classification)
 pwsw_y_full_pred = pwswGNB_full.predict(pwswDF)
 
+# view the predictability accuracy results based on petal and sepal width
 iris.print_classification_report(y_test=pwsw_class_test, y_pred=pwsw_y_test_pred)
 iris.print_classification_report(y_test=iris.classification, y_pred=pwsw_y_full_pred)
 
+# view the decision boundaries of the test and full set based on the petal and sepal width NB
 iris.decision_display(pwswGNB, pwsw_x_test, pwsw_y_test_pred)
 iris.decision_display(pwswGNB_full, pwswDF, pwsw_y_full_pred)
 
+# Now doing the same with sepal length and petal width to be able to compare which categorization may be better
 slpwGNB = GaussianNB().fit(slpw_x_train,slpw_class_train)
-slpw_y_pred = slpwGNB.predict(slpw_x_test)
-iris.print_classification_report(y_test=slpw_class_test, y_pred=slpw_y_pred)
+slpw_y_test_pred = slpwGNB.predict(slpw_x_test)
+slpwGNB_full = GaussianNB().fit(slpwDF, iris.classification)
+slpw_y_full_pred = slpwGNB_full.predict(slpwDF)
+
+iris.print_classification_report(y_test=slpw_class_test, y_pred=slpw_y_test_pred)
+iris.print_classification_report(y_test=iris.classification, y_pred=slpw_y_full_pred)
+
+iris.decision_display(slpwGNB, slpw_x_test, slpw_y_test_pred)
+iris.decision_display(slpwGNB_full, slpwDF, slpw_y_full_pred)
+
+# the precision/recall of sepal length to petal width are better than sepal to petal width
+# sepal length is a more accurate indicator of iris type
